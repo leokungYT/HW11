d05930e6ec4c1ca511301e8dc15810ac82d9390d
กูลีโอ จุฟๆ <111227729+leokungYT@users.noreply.github.com>
2024-01-20 12:27:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ const CounterApp = () => {
   const handleDecrement = (id) => {
     setCounters((prevCounters) =>
       prevCounters.map((counter) =>
-        counter.id === id ? { ...counter, value: counter.value - 1 } : counter
+        counter.id === id
+          ? { ...counter, value: Math.max(counter.value - 1, 0) } // Ensure value doesn't go below 0
+          : counter
       )
     );
   };
